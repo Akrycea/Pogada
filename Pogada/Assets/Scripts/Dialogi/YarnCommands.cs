a1ab7bf8edb3291c395ac81cf55066968801985b
Akrycea <smagibag@gmail.com>
2025-11-16 16:09:45 +0100

    Nie da się pogadać z Granat przed rozmową z Zielonym

--- Pogada/Assets/Scripts/Dialogi/YarnCommands.cs
+++ Pogada/Assets/Scripts/Dialogi/YarnCommands.cs
@@ -4,10 +4,19 @@ using Yarn.Unity;
 
 public class YarnCommands : MonoBehaviour
 {
+
+    //po rozmowie z zielonym pozwala pogadac z granat
+    private ClickDialog granat;
+    [YarnCommand("granatOdpalDialog")]
+    public void granatOdpalaDialog()
+    {
+        granat = GameObject.Find("Granat").GetComponent<ClickDialog>();
+        granat.nazwaDialogu = "D4_PoznanieGranat";
+    }
     //zmienia dialog zielonego po kliknieciu na drugi
     private ClickDialog zielony;
     [YarnCommand("zielony2dialog")]
-    public void green2voice()
+    public void zielony2dialog()
     {
         zielony = GameObject.Find("Zielony").GetComponent<ClickDialog>();
         zielony.nazwaDialogu = "D5_PomocZieleni";
