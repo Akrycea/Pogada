bfebb1b2e12f1869241ea8f0b116b6187d1a3a31
Akrycea <smagibag@gmail.com>
2025-12-04 14:50:42 +0100

    Revert "Merge branch 'main' of https://github.com/Akrycea/Pogada"
    This reverts commit 3a2b541cd1512847810861df32dea6bce18ed101, reversing
    changes made to 7adf3903f9d7a0f97a857779a36e399b49c8d968.

--- Pogada/Assets/Scripts/3Scene/BirdsPuzzle.cs
+++ Pogada/Assets/Scripts/3Scene/BirdsPuzzle.cs
@@ -4,51 +4,34 @@ public class BirdsPuzzle : MonoBehaviour
 {
     public string ObjectName;
     private bool isOnObject;
-    private bool available;
 
     public Vector3 currentPosition;
 
 
     void OnTriggerEnter2D(Collider2D collision)
     {
-        // debug i used for tests
         Debug.Log("Hit collision");
         Debug.Log(collision.gameObject.name);
         Debug.Log(collision.gameObject.transform.position);
  
-
         isOnObject = true;
         currentPosition = collision.gameObject.transform.position;
-        
 
-        if(collision.CompareTag("BirdFree"))
-        {
-            available = true;
-            collision.gameObject.tag = "BirdTaken";
-        }
-        else if (collision.CompareTag("BirdTaken"))
-        {
-            available = false;
-        }
+       
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {   
         isOnObject = false;
-        
-
-        if (available == true)
-        {
-            collision.gameObject.tag = "BirdFree";
-        }
     }
 
     void Update()
     {
-        if (isOnObject == true && available == true && Input.GetMouseButton(0) == false)
+        if (isOnObject == true && Input.GetMouseButton(0) == false)
         {
             gameObject.transform.position = currentPosition;
         }
     }
+
 }
 
