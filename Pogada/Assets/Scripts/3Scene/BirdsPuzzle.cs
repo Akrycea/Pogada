72ee8bbcb2c46b1b48ac13f9f327c440a3235652
ratcase <150330908+ratcase@users.noreply.github.com>
2025-12-04 17:18:12 +0100

    bird minigame

--- Pogada/Assets/Scripts/3Scene/BirdsPuzzle.cs
+++ Pogada/Assets/Scripts/3Scene/BirdsPuzzle.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class BirdsPuzzle : MonoBehaviour
 {
     public string ObjectName;
-    private bool isOnObject;
+    private bool isOnObject = true;
 
     public Vector3 currentPosition;
 
@@ -13,25 +13,37 @@ public class BirdsPuzzle : MonoBehaviour
         Debug.Log("Hit collision");
         Debug.Log(collision.gameObject.name);
         Debug.Log(collision.gameObject.transform.position);
- 
-        isOnObject = true;
-        currentPosition = collision.gameObject.transform.position;
 
-       
+
+
+
+        if (collision.CompareTag("BirdFree"))
+        {
+            isOnObject = true;
+            currentPosition = collision.gameObject.transform.position;
+            collision.gameObject.tag = "BirdTaken";
+        }
+        else if (collision.CompareTag("BirdTaken"))
+        {
+
+        }
     }
 
     void OnTriggerExit2D(Collider2D collision)
-    {   
-        isOnObject = false;
+    {
+        if (isOnObject == true)
+        {
+            collision.gameObject.tag = "BirdFree";
+            isOnObject = false;
+        }
     }
 
     void Update()
     {
-        if (isOnObject == true && Input.GetMouseButton(0) == false)
+        if (Input.GetMouseButton(0) == false)
         {
             gameObject.transform.position = currentPosition;
         }
     }
 
 }
-
