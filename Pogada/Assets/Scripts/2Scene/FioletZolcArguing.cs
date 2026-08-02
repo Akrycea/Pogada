04a8b48cd4ce276dbd54a47ef07ef888e55072ce
Akrycea <smagibag@gmail.com>
2026-08-02 20:27:46 +0200

    Głos Bohatera w pełni wstawiony

--- Pogada/Assets/Scripts/2Scene/FioletZolcArguing.cs
+++ Pogada/Assets/Scripts/2Scene/FioletZolcArguing.cs
@@ -5,9 +5,17 @@ public class FioletZolcArguing : MonoBehaviour
 {
     public DialogueRunner dialogueRunner;
 
+    [YarnCommand("fightCloudDisable")]
+    public void fightCloudDisable()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private SpriteRenderer spriteRenderer;
     [YarnCommand("fightCloudDisappear")]
     public void fightCloudDisappear()
     {
-        gameObject.SetActive(false);
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = false;
     }
 }
