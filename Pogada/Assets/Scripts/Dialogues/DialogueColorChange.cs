eb461064f67f7a97b5dc1c124af52e883d4d8555
ratcase <liseuqgames@gmail.com>
2026-08-30 23:29:21 +0200

    kolory w dialogach!

---
+++ Pogada/Assets/Scripts/Dialogues/DialogueColorChange.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using Yarn.Unity;
+using TMPro;
+using Yarn.Unity.Legacy;
+
+public class DialogueColorChange : LineView
+{
+    //public override void RunLine(LocalizedLine dialogueLine, System.Action onLineComplete)
+    //{
+    //    string text = dialogueLine.TextWithoutCharacterName.Text;
+
+    //    // Automatically wraps the word "Gold" in a TMP color tag
+    //    text = text.Replace("Liv", "<color=#00008B>DarkBlue</color>");
+
+    //    // Send the modified text to your TextMeshPro component
+    //    lineText.text = text;
+
+    //    onLineComplete();
+    //}
+}
+
